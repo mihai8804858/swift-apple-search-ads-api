3a279766d88b68605ab77d93ea41d55a85e133f4
Mihai Șeremet <mihai8804858@gmail.com>
2024-12-30 23:18:03 +0000

    Add Swift 5 package manifest

---
+++ Package@swift-5.10.swift
@@ -0,0 +1,46 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-apple-search-ads-api",
+    platforms: [
+        .iOS(.v16),
+        .tvOS(.v16),
+        .visionOS(.v1),
+        .macOS(.v13),
+        .macCatalyst(.v16),
+        .watchOS(.v9)
+    ],
+    products: [
+        .library(name: "AppleSearchAds", targets: ["AppleSearchAds"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto", from: "3.10.0"),
+        .package(url: "https://github.com/fumoboy007/swift-retry", from: "0.2.4"),
+        .package(url: "https://github.com/CreateAPI/URLQueryEncoder", from: "0.2.1"),
+        .package(url: "https://github.com/pointfreeco/swift-clocks", from: "1.0.6"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.6")
+    ],
+    targets: [
+        .target(
+            name: "AppleSearchAds",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "DMRetry", package: "swift-retry"),
+                .product(name: "URLQueryEncoder", package: "URLQueryEncoder")
+            ],
+            resources: [.copy("Resources/PrivacyInfo.xcprivacy")]
+        ),
+        .testTarget(
+            name: "AppleSearchAdsTests",
+            dependencies: [
+                .target(name: "AppleSearchAds"),
+                .product(name: "Clocks", package: "swift-clocks"),
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+                .product(name: "InlineSnapshotTesting", package: "swift-snapshot-testing")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
