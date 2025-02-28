20974f6dc95fd2f4e9c37603e27dca95ad5010a6
Mihai Șeremet <mihai8804858@gmail.com>
2025-02-28 22:21:17 +0000

    Decode gender and device class case insensitive

--- Sources/Core/Models/Codable/DeviceClass.swift
+++ Sources/Core/Models/Codable/DeviceClass.swift
@@ -4,4 +4,17 @@ public enum DeviceClass: String, Codable, Hashable, Sendable {
     case iPhone = "IPHONE"
     /// The iPad device class.
     case iPad = "IPAD"
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawString = try container.decode(String.self)
+        if let deviceClass = DeviceClass(rawValue: rawString.uppercased()) {
+            self = deviceClass
+        } else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Cannot initialize \(Self.self) from invalid String value \(rawString)"
+            )
+        }
+    }
 }
--- Sources/Core/Models/Codable/Gender.swift
+++ Sources/Core/Models/Codable/Gender.swift
@@ -4,4 +4,17 @@ public enum Gender: String, Codable, Hashable, Sendable {
     case male = "M"
     /// Female.
     case female = "F"
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawString = try container.decode(String.self)
+        if let gender = Gender(rawValue: rawString.uppercased()) {
+            self = gender
+        } else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Cannot initialize \(Self.self) from invalid String value \(rawString)"
+            )
+        }
+    }
 }
